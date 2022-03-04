7efb69f0115fc30c7f594e8a442783866f75da85
madpgmer <madpgmer@gmail.com>
2022-03-03 17:45:29 -0700

    Added User and Role class
    -Not manipulating any roles in RoleDB.java so implemented just getAll() method
    - Added Insert/Delete/Update/getAll methods in UserDB

--- Lab6JDBC/src/main/java/ca/sait/lab6jdbc/dataaccess/UserDB.java
+++ Lab6JDBC/src/main/java/ca/sait/lab6jdbc/dataaccess/UserDB.java
@@ -1,5 +1,6 @@
 package ca.sait.lab6jdbc.dataaccess;
 
+import ca.sait.lab6jdbc.models.Role;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,25 +10,31 @@ import ca.sait.lab6jdbc.models.User;
 
 public class UserDB {
 
-    public List<User> getAll(String owner) throws Exception {
-        List<User> notes = new ArrayList<>();
+    public List<User> getAll() throws Exception {
+        List<User> users = new ArrayList<>();
         ConnectionPool cp = ConnectionPool.getInstance();
         Connection con = cp.getConnection();
         PreparedStatement ps = null;
         ResultSet rs = null;
         
-        String sql = "SELECT * FROM note WHERE owner=?";
+        String sql = "SELECT * FROM user INNER JOIN role ON role.role_id = user.role";
         
         try {
             ps = con.prepareStatement(sql);
-            ps.setString(1, owner);
             rs = ps.executeQuery();
+            
             while (rs.next()) {
-                int noteId = rs.getInt(1);
-                String title = rs.getString(2);
-                String contents = rs.getString(3);
-                User note = new User(noteId, title, contents, owner);
-                notes.add(note);
+                String email = rs.getString(1);
+                boolean active = rs.getBoolean(2);
+                String firstName = rs.getString(3);
+                String lastName = rs.getString(4);
+                String password = rs.getString(5);
+                int roleId = rs.getInt(6);
+                String roleName = rs.getString(7);
+                
+                Role role = new Role(roleId, roleName);
+                User user = new User(email, active, firstName, lastName,password, role );
+                users.add(user);
             }
         } finally {
             DBUtil.closeResultSet(rs);
@@ -35,26 +42,31 @@ public class UserDB {
             cp.freeConnection(con);
         }
 
-        return notes;
+        return users;
     }
 
-    public Note get(int noteId) throws Exception {
-        Note note = null;
+    public User get(String email) throws Exception {
+        User user = null;
         ConnectionPool cp = ConnectionPool.getInstance();
         Connection con = cp.getConnection();
         PreparedStatement ps = null;
         ResultSet rs = null;
-        String sql = "SELECT * FROM note WHERE note_id=?";
+        String sql = "SELECT * FROM user INNER JOIN role ON role.role_id = user.role WHERE email =?";
         
         try {
             ps = con.prepareStatement(sql);
-            ps.setInt(1, noteId);
+            ps.setString(1, email);
             rs = ps.executeQuery();
             if (rs.next()) {
-                String title = rs.getString(2);
-                String contents = rs.getString(3);
-                String owner = rs.getString(4);
-                note = new Note(noteId, title, contents, owner);
+                boolean active = rs.getBoolean(2);
+                String firstName = rs.getString(3);
+                String lastName = rs.getString(4);
+                String password = rs.getString(5);
+                int roleId = rs.getInt(6);
+                String roleName = rs.getString(7);
+                
+                Role role = new Role(roleId, roleName);
+                user = new User(email, active, firstName, lastName,password, role );
             }
         } finally {
             DBUtil.closeResultSet(rs);
@@ -62,59 +74,68 @@ public class UserDB {
             cp.freeConnection(con);
         }
         
-        return note;
+        return user;
     }
 
-    public void insert(Note note) throws Exception {
+    public boolean insert(User user) throws Exception {
         ConnectionPool cp = ConnectionPool.getInstance();
         Connection con = cp.getConnection();
         PreparedStatement ps = null;
-        String sql = "INSERT INTO note (title, contents, owner) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO user (`email`, `first_name`, `last_name`, `password`, `role`) VALUES (?, ?, ?, ?, ?);";
         
+        boolean inserted = false;
         try {
             ps = con.prepareStatement(sql);
-            ps.setString(1, note.getTitle());
-            ps.setString(2, note.getContents());
-            ps.setString(3, note.getOwner());
-            ps.executeUpdate();
+            ps.setString(1, user.getEmail());
+            ps.setString(2, user.getFirstname());
+            ps.setString(3, user.getLastname());
+            ps.setString(4, user.getPassword());
+            ps.setInt(5, user.getRole().getId());
+            inserted = ps.executeUpdate() != 0 ? true : false;
         } finally {
             DBUtil.closePreparedStatement(ps);
             cp.freeConnection(con);
         }
+        return inserted;
     }
 
-    public void update(Note note) throws Exception {
+    public boolean update(User user) throws Exception {
         ConnectionPool cp = ConnectionPool.getInstance();
         Connection con = cp.getConnection();
         PreparedStatement ps = null;
-        String sql = "UPDATE note SET title=?, contents=? WHERE note_id=?";
-        
+        String sql = "UPDATE user SET `first_name`= ?, `last_name`= ?, `password`= ?, `role`= ? WHERE  `email`= ?";
+        boolean updated = false;
         try {
             ps = con.prepareStatement(sql);
-            ps.setString(1, note.getTitle());
-            ps.setString(2, note.getContents());
-            ps.setInt(3, note.getNoteId());
-            ps.executeUpdate();
+            ps.setString(1, user.getFirstname());
+            ps.setString(2, user.getLastname());
+            ps.setString(3, user.getPassword());
+            ps.setInt(4, user.getRole().getId());
+            ps.setString(5, user.getEmail());
+            
+            updated = ps.executeUpdate()!= 0;
         } finally {
             DBUtil.closePreparedStatement(ps);
             cp.freeConnection(con);
         }
+        return updated;
     }
 
-    public void delete(Note note) throws Exception {
+    public boolean delete(User user) throws Exception {
         ConnectionPool cp = ConnectionPool.getInstance();
         Connection con = cp.getConnection();
         PreparedStatement ps = null;
-        String sql = "DELETE FROM note WHERE note_id=?";
-        
+        String sql = "DELETE FROM user WHERE email =?";
+        boolean deleted = false;
         try {
             ps = con.prepareStatement(sql);
-            ps.setInt(1, note.getNoteId());
-            ps.executeUpdate();
+            ps.setString(1, user.getEmail());
+            deleted = ps.executeUpdate()!= 0;
         } finally {
             DBUtil.closePreparedStatement(ps);
             cp.freeConnection(con);
         }
+        return deleted;
     }
 
 }
